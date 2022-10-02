a4369a7a406de25d8e3e99abf48e9c45856bf71c
Avihoo Ilan <AvihooI@Gmail.com>
2022-10-02 16:18:26 +0300

    wip

--- examples/example_1/example_driver.py
+++ examples/example_1/example_driver.py
@@ -2,3 +2,4 @@ from generichtf.core import TestSuite
 
 if __name__ == '__main__':
     test_suite = TestSuite('test_suite')
+
--- generichtf/core.py
+++ generichtf/core.py
@@ -5,6 +5,7 @@ import sys
 from inspect import isclass
 from typing import Callable, List
 from dataclasses import dataclass
+from threading import Thread
 
 from generichtf.base import ToolFactory, TestEnvironment
 from generichtf.exceptions import *
@@ -18,6 +19,11 @@ class TestProcedureEntry:
     procedure: Callable
 
 
+class ProcedureRunner:
+    def __init__(self, procedure_entry: TestProcedureEntry):
+        self.procedure_entry = TestProcedureEntry
+
+
 class TestSuite:
     def _register_procedure(self, name: str, parameters=None, outputs=None):
         parameters = parameters if parameters else []
@@ -29,6 +35,14 @@ class TestSuite:
             return test_function
 
         return inner_register_procedure
+
+    def _register_flow(self, name):
+
+        def inner_register_flow(flow_function: Callable):
+            self.flows[name] = flow_function
+            return flow_function
+
+        return inner_register_flow
 
     def _load_tool_file(self, tool_file_path: str):
         module_name = split(tool_file_path)[-1].removesuffix('.py')
@@ -69,6 +83,19 @@ class TestSuite:
         except Exception as e:
             raise e
 
+    def _load_flows_file(self, flows_file_path: str):
+        module_name = split(flows_file_path)[-1].removesuffix('.py')
+        spec = importlib.util.spec_from_file_location(module_name, flows_file_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        module.__dict__['register_flow'] = self._register_flow
+
+        try:
+            # run the module
+            spec.loader.exec_module(module)
+        except Exception as e:
+            raise e
+
     @staticmethod
     def _load_module_files(modules_dir_path: str, module_loading_function: Callable):
         module_file_paths = [join(modules_dir_path, filename) for filename in listdir(modules_dir_path) if
@@ -81,14 +108,18 @@ class TestSuite:
         self.env = TestEnvironment()
         self.tools = dict()
         self.procedures = dict()
+        self.flows = dict()
 
         dir_list = [dir_name for dir_name in listdir(root_dir) if isdir(join(root_dir, dir_name))]
-        assert ('procedures' in dir_list and 'tools' in dir_list)
+        assert ('procedures' in dir_list and 'tools' in dir_list and 'flows' in dir_list)
 
         tools_dir_path = join(root_dir, 'tools')
         self._load_module_files(tools_dir_path, self._load_tool_file)
 
         procedures_dir_path = join(root_dir, 'procedures')
         self._load_module_files(procedures_dir_path, self._load_procedures_file)
+
+        flows_dir_path = join(root_dir, 'flows')
+        self._load_module_files(flows_dir_path, self._load_flows_file)
 
         pass
