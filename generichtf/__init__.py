5581454f37c963625a2c580c14bd281290fd0a10
Avihoo Ilan <AvihooI@Gmail.com>
2023-02-08 19:01:10 +0200

    wip add procedure status to main module

--- generichtf/__init__.py
+++ generichtf/__init__.py
@@ -1,4 +1,4 @@
 # TODO: split into modules
 
 from .core import TestSuite
-from .base import TestSession, TestSuiteView, TestSessionStatus
+from .base import TestSession, TestSuiteView, TestSessionStatus, ProcedureStatus
